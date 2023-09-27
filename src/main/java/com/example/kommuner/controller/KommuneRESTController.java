4a7e108e01f5740105acc81dd01b058c4cb71456
math92n4 <math92n4@stud.kea.dk>
2023-09-27 11:57:31 +0200

    commit

--- src/main/java/com/example/kommuner/controller/KommuneRESTController.java
+++ src/main/java/com/example/kommuner/controller/KommuneRESTController.java
@@ -1,15 +1,21 @@
 package com.example.kommuner.controller;
 
+import com.example.kommuner.exception.ResourceNotFoundException;
 import com.example.kommuner.model.Kommune;
 import com.example.kommuner.repository.KommuneRepository;
 import com.example.kommuner.service.ApiServiceGetKommuner;
 import com.example.kommuner.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -32,6 +38,34 @@ public class KommuneRESTController {
         return apiServiceGetKommuner.getKommuner();
     }
 
+    @GetMapping("/kommunepage")
+    public ResponseEntity<List<Kommune>> getKommuneInPages() {
+        int page = 4;
+        int size = 5;
+        Pageable paging = PageRequest.of(page, size);
+        Page<Kommune> pageKommuner = kommuneRepository.findAll(paging);
+        List<Kommune> kommuner = pageKommuner.getContent();
+        return new ResponseEntity<>(kommuner, HttpStatus.OK);
+    }
+
+    @GetMapping("/kommunepageparam")
+    public ResponseEntity<Map<String, Object>> getPageOfKommuner(@RequestParam(defaultValue = "0") int page,
+                                                                 @RequestParam(defaultValue = "10") int size) {
+        Pageable paging = PageRequest.of(page,size);
+        Page<Kommune> pageKommuner = kommuneRepository.findAll(paging);
+        List<Kommune> kommuner = pageKommuner.getContent();
+
+        if (kommuner.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        Map<String, Object> response = new HashMap<>();
+        response.put("kommuner", kommuner);
+        response.put("currentPage", pageKommuner.getNumber());
+        response.put("totalItems", pageKommuner.getTotalElements());
+        response.put("totalPages", pageKommuner.getTotalPages());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
     @GetMapping("/kommune/{kode}")
     public ResponseEntity<Kommune> searchKommune(@PathVariable String kode) {
         Kommune kommune = kommuneRepository.findByKode(kode);
@@ -40,6 +74,13 @@ public class KommuneRESTController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/kommunenavn/{navn}")
+    public ResponseEntity<Kommune> getKommuneByNavn(@PathVariable String navn) {
+        var kommune = kommuneRepository.findKommuneByNavn(navn).orElseThrow(() ->
+                new ResourceNotFoundException("Kommune ikke fundet med navn: " + navn));
+        return new ResponseEntity<>(kommune, HttpStatus.OK);
     }
 
     @PostMapping("/addkommune")
--- src/main/java/com/example/kommuner/repository/KommuneRepository.java
+++ src/main/java/com/example/kommuner/repository/KommuneRepository.java
@@ -3,6 +3,10 @@ package com.example.kommuner.repository;
 import com.example.kommuner.model.Kommune;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface KommuneRepository extends JpaRepository<Kommune,String> {
+    // JPQL
     Kommune findByKode(String kode);
+    Optional<Kommune> findKommuneByNavn(String navn);
 }
